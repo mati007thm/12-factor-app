197c96b7541fb6b57a0d69388990d17c24c3132f
Matthias Theuermann <mtheuermann@infralovers.com>
2024-07-26 21:04:52 +0200

    feat: added keda for scaling
    Signed-off-by: Matthias Theuermann <mtheuermann@infralovers.com>

--- dapr-distributed-calendar/kubernetes-local-deploy-min.sh
+++ dapr-distributed-calendar/kubernetes-local-deploy-min.sh
@@ -4,54 +4,55 @@
 kubectl create namespace 12-factor-app
 
 # install OTel Operator
-kubectl create namespace opentelemetry
-helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
-helm repo update
-helm install my-opentelemetry-operator open-telemetry/opentelemetry-operator \
-  --set admissionWebhooks.certManager.enabled=false \
-  --set admissionWebhooks.certManager.autoGenerateCert.enabled=true \
-  --set manager.featureGates='operator.autoinstrumentation.go' \
-  --namespace opentelemetry \
-  --wait
+# Breaking changes to the OTEL operator helm chart need to review the changes
+# kubectl create namespace opentelemetry
+# helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+# helm repo update
+# helm install my-opentelemetry-operator open-telemetry/opentelemetry-operator \
+#   --set "manager.collectorImage.repository=otel/opentelemetry-collector-k8s" \
+#   --set admissionWebhooks.certManager.enabled=false \
+#   --namespace opentelemetry \
+#   --create-namespace \
+#   --wait
 
 # create OTel collector and instrumentation
-kubectl apply -f otel/.
+# kubectl apply -f otel/.
 
 # install cert-manager OPTIONAL
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-helm upgrade --install \
-  cert-manager jetstack/cert-manager \
-  --namespace cert-manager \
-  --create-namespace \
-  --version v1.7.1 \
-  --set installCRDs=true \
-  --wait
+# helm repo add jetstack https://charts.jetstack.io
+# helm repo update
+# helm upgrade --install \
+#   cert-manager jetstack/cert-manager \
+#   --namespace cert-manager \
+#   --create-namespace \
+#   --version v1.7.1 \
+#   --set installCRDs=true \
+#   --wait
 
 # install jaeger (requires cert-manager) OPTIONAL
-kubectl create namespace observability
-kubectl create -f https://github.com/jaegertracing/jaeger-operator/releases/download/v1.38.0/jaeger-operator.yaml -n observability
-kubectl wait --for=condition=ready pod --all --timeout=200s -n observability
-kubectl apply -f jaeger/.
+# kubectl create namespace observability
+# kubectl create -f https://github.com/jaegertracing/jaeger-operator/releases/download/v1.38.0/jaeger-operator.yaml -n observability
+# kubectl wait --for=condition=ready pod --all --timeout=200s -n observability
+# kubectl apply -f jaeger/.
 
 # install prometheus OPTIONAL
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install prometheus prometheus-community/kube-prometheus-stack \
-        --namespace observability \
-        --values prometheus/kube-prometheus-stack-values.yaml \
-        --wait
-kubectl apply -f ./prometheus/ingress.yaml
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo update
+# helm install prometheus prometheus-community/kube-prometheus-stack \
+#         --namespace observability \
+#         --values prometheus/kube-prometheus-stack-values.yaml \
+#         --wait
+# kubectl apply -f ./prometheus/ingress.yaml
 
 # install elastic (requires namespace 'observability') OPTIONAL
-helm repo add elastic https://helm.elastic.co
-helm repo update
-helm install elasticsearch elastic/elasticsearch --version 7.17.3 -n observability --set replicas=1 --wait
-helm install kibana elastic/kibana --version 7.17.3 -n observability --wait
-kubectl apply -f ./fluent/fluentd-config-map.yaml
-kubectl apply -f ./fluent/fluentd-dapr-with-rbac.yaml
-kubectl wait --for=condition=ready pod --all --timeout=200s -n observability
-kubectl apply -f ./fluent/ingress.yaml
+# helm repo add elastic https://helm.elastic.co
+# helm repo update
+# helm install elasticsearch elastic/elasticsearch --version 7.17.3 -n observability --set replicas=1 --wait
+# helm install kibana elastic/kibana --version 7.17.3 -n observability --wait
+# kubectl apply -f ./fluent/fluentd-config-map.yaml
+# kubectl apply -f ./fluent/fluentd-dapr-with-rbac.yaml
+# kubectl wait --for=condition=ready pod --all --timeout=200s -n observability
+# kubectl apply -f ./fluent/ingress.yaml
 
 # install dapr
 helm repo add dapr https://dapr.github.io/helm-charts/
@@ -65,7 +66,6 @@ helm upgrade --install dapr dapr/dapr \
 # install dapr dashboard OPTIONAL
 helm install dapr-dashboard dapr/dapr-dashboard --namespace dapr-system --wait
 
-# install KEDA for scaling
 helm repo add kedacore https://kedacore.github.io/charts
 helm repo update
 helm install keda kedacore/keda --namespace keda --create-namespace --wait
@@ -75,7 +75,7 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm install redis bitnami/redis --namespace 12-factor-app --values redis/values.yaml --wait
 
-# set redis password as secret for kedascaledobject
+# deploy the 12-factor-app
 redis_encoded_pwd=$(kubectl get secret redis -n 12-factor-app -o jsonpath='{.data.redis-password}')
 kubectl apply -f - <<EOF
 apiVersion: v1
@@ -88,16 +88,14 @@ data:
   redis_username: ""
   redis_password: $redis_encoded_pwd
 EOF
-
-# deploy the 12-factor-app
 kubectl apply -f kubernetes/.
-kubectl wait --for=condition=ready pod --all --timeout=200s -n 12-factor-app
+# kubectl wait --for=condition=ready pod --all --timeout=200s -n 12-factor-app
 
 # setup OpenCost for cost monitoring OPTIONAL
-kubectl create namespace opencost
-helm install opencost --repo https://opencost.github.io/opencost-helm-chart opencost \
-  --namespace opencost -f open-cost/local.yaml --wait
-kubectl apply -f open-cost/ingress.yaml
+# kubectl create namespace opencost
+# helm install opencost --repo https://opencost.github.io/opencost-helm-chart opencost \
+#   --namespace opencost -f open-cost/local.yaml --wait
+# kubectl apply -f open-cost/ingress.yaml
 
 # setup kubecost for cost monitoring OPTIONAL
 # helm upgrade --install kubecost \
--- dapr-distributed-calendar/locust/main.py
+++ dapr-distributed-calendar/locust/main.py
@@ -1,4 +1,5 @@
 from locust import HttpUser, task, between
+import random
 
 default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
@@ -7,7 +8,11 @@ class EventsUser(HttpUser):
     
     @task
     def event_lifecycle(self):
-        for event_id in range(100, 105):
+        user_id = random.random() + random.random() * random.random()
+
+        for i in range(5):
+            event_id = user_id * 1000 + i  # Create a unique event ID based on the user ID
+
             # Create an event
             headers = {
                 'Content-Type': 'application/json',
@@ -22,13 +27,14 @@ class EventsUser(HttpUser):
             }
 
             # Send the POST request to create an event
-            response = self.client.post(f'/newevent', json=data, headers=headers)
+            response = self.client.post('/newevent', json=data, headers=headers)
 
             # Check if the request was successful
             if response.status_code == 200:
                 print(f"Event {event_id} created successfully")
             else:
                 print(f"Failed to create event {event_id}. Status code: {response.status_code}")
+                continue  # Skip the rest of the loop for this event_id
             
             # Get the event
             response = self.client.get(f'/event/{event_id}')
@@ -38,6 +44,7 @@ class EventsUser(HttpUser):
                 print(f"Event {event_id} retrieved successfully")
             else:
                 print(f"Failed to retrieve event {event_id}. Status code: {response.status_code}")
+                continue  # Skip the rest of the loop for this event_id
 
             # Update the event
             updated_data = {
@@ -53,6 +60,7 @@ class EventsUser(HttpUser):
                 print(f"Event {event_id} updated successfully")
             else:
                 print(f"Failed to update event {event_id}. Status code: {response.status_code}")
+                continue  # Skip the rest of the loop for this event_id
 
             # Delete the event
             response = self.client.delete(f'/event/{event_id}')
@@ -62,3 +70,6 @@ class EventsUser(HttpUser):
                 print(f"Event {event_id} deleted successfully")
             else:
                 print(f"Failed to delete event {event_id}. Status code: {response.status_code}")
+
+        # Stop the user after completing the lifecycle
+        # raise StopUser
